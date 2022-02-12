adfdd3106ad47d0f8c18eb960c6596344c90aa6a
Shubham  Aher <84302530+Shubhamaher1@users.noreply.github.com>
2022-02-12 23:04:43 +0530

    top 3 list of Filtered List

--- src/App.js
+++ src/App.js
@@ -1,10 +1,62 @@
-import "./styles.css";
+import React, { Component, useState } from "react";
+// import "../styles/App.css";
 
-export default function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+const tempArr = [];
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.cityList = [
+      { name: "Goa", country: "India" },
+      { name: "Amsterdam", country: "Netherlands" },
+      { name: "New York", country: "USA" },
+      { name: "Darjeeling", country: "India" },
+      { name: "Tokyo", country: "Japan" },
+      { name: "Lonavala", country: "India" },
+      { name: "Brandenburg Gate", country: "Germany" },
+      { name: "Reichstag Building", country: "Germany" },
+      { name: "Museum Island", country: "Germany" },
+      { name: "Munnar", country: "India" },
+      { name: "Leh Ladakh", country: "India" },
+      { name: "Goa", country: "India" },
+      { name: "Agra", country: "India" },
+      { name: "Dalhousie", country: "India" },
+      { name: "Coorg", country: "India" },
+      { name: "Rome", country: "Italy" },
+      { name: "Milan", country: "Italy" },
+      { name: "Venice", country: "Italy" },
+      { name: "Varanasai", country: "India" },
+      { name: "Jaipur", country: "India" },
+      { name: "The Hofburg", country: "Austria" },
+      { name: "Belvedere Palace", country: "Austria" },
+      { name: "St. Stephen Cathedral", country: "Austria" },
+      { name: "Kahna National Park", country: "India" },
+      { name: "Amritsar", country: "India" },
+      { name: "Mussoorie", country: "India" },
+      { name: "Mount Abu", country: "India" },
+      { name: "Tirupati", country: "India" }
+    ];
+  }
+
+  render() {
+    this.tempArr = this.cityList.filter((ele, index) => {
+       return ele.country=="India";
+    });
+
+    return (
+      <div id="main">
+        <ol>
+          {this.tempArr.map((ele, index) => {
+           if(index<3){
+            return <li key={`location${index + 1}`}>{ele.name}</li>;
+           }
+          })}
+        </ol>
+        {/* Do not remove the main div */}
+      </div>
+    );
+  }
 }
+
+export default App;
+//
